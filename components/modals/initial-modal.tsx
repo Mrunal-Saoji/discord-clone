5d2464eec30f213c9ec980873fb0e904b27f1d03
Mrunal Saoji <mrunalsaoji@gmail.com>
2023-09-15 17:58:31 +0000

    working on sidebar

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import axios from "axios"
 import * as z from "zod";
 import {zodResolver} from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form";
@@ -27,6 +27,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 
 const formSchema = z.object({
@@ -41,7 +42,8 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
 
-    const [isMounted,setIsMounted] = useState(false)
+    const [isMounted,setIsMounted] = useState(false);
+    const router = useRouter();
 
     useEffect(()=>{
         setIsMounted(true)
@@ -58,7 +60,17 @@ export const InitialModal = () => {
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = async(values:z.infer<typeof formSchema>)=>{
-        console.log(values);
+        try{
+
+            await axios.post("/api/servers",values);
+            form.reset();
+            router.refresh();
+            window.location.reload()
+
+
+        }catch(error) {
+            console.log(error)
+        }
     }
 
     if(!isMounted){
