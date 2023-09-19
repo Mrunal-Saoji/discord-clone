6a8a32585a323eac96aca52516589e3e10e292bb
Mrunal Saoji <mrunalsaoji@gmail.com>
2023-09-19 16:47:43 +0000

    added server creation button

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,6 +1,8 @@
 import { currentProfile } from "@/lib/current-profile"
 import { redirect } from "next/navigation"
 import { db } from "@/lib/db"
+import { NavigationAction } from "./navigation-action"
+import { Separator } from "../ui/separator"
 
 export const NavigationSidebar = async() => {
     const profile = await currentProfile()
@@ -22,7 +24,10 @@ export const NavigationSidebar = async() => {
 
     return (
         <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
-            Navigation Sidebar
+           <NavigationAction />
+           <Separator 
+            className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"
+           />
         </div>
     )
 }
